fcd16c5b2b0cba92770535c602f9c20820a7c662
HumphreyJ <joshhumphrey1@gmail.com>
2019-12-17 23:26:31 -0600

    Met Mvp work on stretch tomorrow.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
 
 // Components
 import Navigation from './components/Navigation';
@@ -12,29 +14,31 @@ function App() {
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
-		// add the given item to the cart
+		setCart([...cart,item])
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{products, addItem}}>
+				<CartContext.Provider value= {{cart}}>
+					<Navigation cart={cart} />
+						{/* Routes */}
+						<Route
+							exact
+							path="/"
+							component={Products}
+								/>
+						
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+						<Route
+							path="/cart"
+							component={ShoppingCart}
+						/>
+				</CartContext.Provider>
+			
+			</ProductContext.Provider>
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
+						
 		</div>
 	);
 }
